6798c363f8c1196ca6a4acf874dc481965a40a6c
nateglims <nglimsdale@gmail.com>
2023-12-06 19:50:42 -0800

    Redo day, part 2 without regex

--- day-one/src/lib.rs
+++ day-one/src/lib.rs
@@ -1,5 +1,3 @@
-use regex::Regex;
-
 // Part 1
 pub fn part1(input: Vec<String>) -> i64 {
     let mut sum: i64 = 0;
@@ -26,59 +24,55 @@ pub fn part1(input: Vec<String>) -> i64 {
 // Part 2... Oof...
 pub fn part2(input: Vec<String>) -> i64 {
     let mut sum: i64 = 0;
-    // Run this first:
-    // sed -e s/oneight/oneeight/ \
-    //  -e s/twone/twoone/ \
-    //  -e s/threeight/threeeight/ \
-    //  -e s/fiveight/fiveeight/ \
-    //  -e s/sevenine/sevennine/ \
-    //  -e s/eightwo/eighttwo/ \
-    //  -e s/eighthree/eightthree/ \
-    //  -e s/nineight/nineeight/ \
-    //  day1.input > treated.input
-
-    let re = Regex::new(r"(one|two|three|four|five|six|seven|eight|nine|ten|\d)")
-        .expect("regex init failed.");
-
     for line in input {
-        let mut caps = re.captures_iter(line.as_str()).map(|c| c.extract());
-
-        let (_, [first]) = caps.nth(0).expect("first :(");
-
-        let (_, [last]) = caps.last().unwrap_or(("", [first]));
-
-        let val = get_val(first) * 10 + get_val(last);
-        //        println!("{} + {} = {}", first, last, val);
-
-        sum += val;
+        let mut nums = Vec::new();
+        for idx in 0..line.len() {
+            let s = &line[idx..];
+            if let Some(n) = match s.get(0..1) {
+                Some("0") => Some(0),
+                Some("1") => Some(1),
+                Some("2") => Some(2),
+                Some("3") => Some(3),
+                Some("4") => Some(4),
+                Some("5") => Some(5),
+                Some("6") => Some(6),
+                Some("7") => Some(7),
+                Some("8") => Some(8),
+                Some("9") => Some(9),
+                _ => None,
+            } {
+                nums.push(n);
+                continue;
+            } else if let Some(n) = match s.get(0..3) {
+                Some("one") => Some(1),
+                Some("two") => Some(2),
+                Some("six") => Some(6),
+                _ => None,
+            } {
+                nums.push(n);
+                continue;
+            } else if let Some(n) = match s.get(0..4) {
+                Some("four") => Some(4),
+                Some("five") => Some(5),
+                Some("nine") => Some(9),
+                _ => None,
+            } {
+                nums.push(n);
+                continue;
+            } else if let Some(n) = match s.get(0..5) {
+                Some("seven") => Some(7),
+                Some("three") => Some(3),
+                Some("eight") => Some(8),
+                _ => None,
+            } {
+                nums.push(n);
+                continue;
+            }
+        }
+        sum += nums.first().unwrap() * 10 + nums.last().unwrap();
     }
 
     sum
-}
-
-fn get_val(s: &str) -> i64 {
-    match s {
-        "0" => 0,
-        "1" => 1,
-        "2" => 2,
-        "3" => 3,
-        "4" => 4,
-        "5" => 5,
-        "6" => 6,
-        "7" => 7,
-        "8" => 8,
-        "9" => 9,
-        "one" => 1,
-        "two" => 2,
-        "three" => 3,
-        "four" => 4,
-        "five" => 5,
-        "six" => 6,
-        "seven" => 7,
-        "eight" => 8,
-        "nine" => 9,
-        _ => unreachable!(":("),
-    }
 }
 
 #[cfg(test)]
@@ -112,9 +106,9 @@ mod tests {
     }
 
     #[test]
-    fn why_is_this_missing() {
-        let input = ["6278teight3three"].map(String::from).to_vec();
+    fn special_case() {
+        let input = ["oneight"].map(String::from).to_vec();
 
-        assert_eq!(part2(input), 63);
+        assert_eq!(part2(input), 18);
     }
 }
